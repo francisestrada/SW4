62d8c10e409953b2d90c108e50ea082fd9f6c7ea
francisestrada <francis.estrada29@yahoo.com.ph>
2014-08-14 10:34:46 +0800

    recommit
    wrong extension file

---
+++ sw10.js
@@ -0,0 +1,8 @@
+function capitalise(string) {
+	var i = string.length
+	    console.log (string.charAt(0).toUpperCase() + string.slice(1).toLowerCase());
+}
+capitalise("alfredo")  
+capitalise("alejandro")
+capitalise("aLBERTO")  
+capitalise("arMaNdO")  
---
+++ sw3.js
@@ -0,0 +1,10 @@
+var min = function(i,j){
+	if (i<j){
+		console.log(i);
+	}
+	else{
+		consle.log(j);
+
+	}
+}
+min(2,3);
